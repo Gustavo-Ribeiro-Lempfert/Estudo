ae208dd674e2746a365d29ae499c87096233b518
Gustavo Ribeiro Lempfert <gustavoribe00@outlook.com>
2023-03-25 16:24:50 -0300

    importa projeto para validação de formularios

--- est js/Requisições e validação/aluraplay-requisicoes-main/js/conectaApi.js
+++ est js/Requisições e validação/aluraplay-requisicoes-main/js/conectaApi.js
@@ -1,10 +1,9 @@
 async function listaVideos() {
-    const conexao = await fetch("http://localhost:3000/videos")
-    const conexaoConvertida =await conexao.json(); 
-    return conexaoConvertida
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 }
-
-
 
 async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
@@ -17,18 +16,22 @@ async function criaVideo(titulo, descricao, url, imagem) {
             descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
-        })        
+        })
     });
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+    const conexaoConvertida = conexao.json();
 
-    const conexaoConvertida = await conexao.JSON();
-    return conexaoConvertida
+    return conexaoConvertida;
 }
 
 async function buscaVideo(termoDeBusca) {
-    const conexao =await fetch (`http://localhost:3000/videos?q=${termoDeBusca}`)
-    const conexaoConvertida = conexao.json();
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
+
     return conexaoConvertida;
-}   
+}
 
 export const conectaApi = {
     listaVideos,
