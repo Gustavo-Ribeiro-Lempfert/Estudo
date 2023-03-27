2bcbfbb15183e3e44c3fe192e3550b72c0fbf306
Gustavo Ribeiro Lempfert <gustavoribe00@outlook.com>
2023-03-27 15:54:55 -0300

    implementada a criação de negociações

--- typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,24 +1,17 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
 
-    constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
-    }
+    constructor(
+        private _data: Date,
+        readonly quantidade: number,
+        readonly valor: number
+        ) {}
 
-    get data(){
-        return this.#data
-    }
-    get quantidade(){
-        return this.#quantidade
-    }
-    get valor(){
-        return this.#valor
-    }
     get volume(){
-        return this.#quantidade * this.valor
+        return this.quantidade * this.valor
+    }
+
+    get data(): Date{
+        const data = new Date(this._data.getTime())
+        return this._data
     }
 }
---
+++ typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociecoes.js
@@ -0,0 +1,2 @@
+export class negociacoes {
+}
