bfad295ac35f0188a28ff5b77e5a12dfd98b5a84
Gustavo Ribeiro Lempfert <gustavoribe00@outlook.com>
2023-04-03 10:26:22 -0300

    Criado o primeiro componente em Angular

---
+++ Fundamentos-Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeiroComponentComponent } from './primeiro-componente/primeiro-component.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponentComponent,
+    SegundoComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
